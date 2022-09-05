ef1486e60d32f32f16871bd0a168bd304a80a07c
md1024 <mdev1024@mail.com>
2022-09-05 17:09:01 -0400

    Update ir_receiver.ino
    adding code to read raw data from remote.

--- ac_control/ir_receiver/ir_receiver.ino
+++ ac_control/ir_receiver/ir_receiver.ino
@@ -1,3 +1,5 @@
+#define sprint Serial.print 
+#define sprintln Serial.println
 #include <IRremote.h>
 int RECV_PIN = 11;
 
@@ -6,6 +8,7 @@ IRrecv irrecv(RECV_PIN);
 
 
 decode_results results;
+int c = 1;
 
 void setup()
 {
@@ -17,16 +20,47 @@ void loop() {
 
 
   if (irrecv.decode(&results)) {
-   
+   dump(&results);
+// ========
     Serial.println(results.value, HEX);
-    
+    Serial.print("rawlen ");
+    Serial.println(results.rawlen);
+    Serial.print("decode type ");
+    Serial.println(results.decode_type);
     if(results.value==0xA1026EFF) {
       Serial.println("turning off");
-      irsend.sendNEC(off_code,off_code_len);
     }
     irrecv.resume(); // Receive the next value
 
   }
 }
 
-//
+void dump(decode_results *results) {
+   int count = results->rawlen;
+   sprintln(c);
+   c++;
+   sprintln("For IR Scope: ");
+   for (int i = 1; i < count; i++) {
+       sprint("0x");
+       sprint((unsigned int)results->rawbuf[i], HEX);
+    sprint(" ");
+   }
+
+   sprintln("");
+   sprintln("For Arduino sketch: ");
+   sprint("unsigned int raw[");
+   sprint(count, DEC);
+   sprint("] = {");
+   for (int i = 1; i < count; i++) {
+       sprint("0x");
+       sprint((unsigned int)results->rawbuf[i], HEX);
+       sprint(",");
+    }
+    sprint("};");
+    sprintln("");
+    sprint("irsend.sendRaw(raw,");
+    sprint(count, DEC);
+    sprint(",38);");
+    sprintln("");
+    sprintln("");
+}
